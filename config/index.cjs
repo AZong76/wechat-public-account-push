ec7a3d7dcbf6a4361addd0f670738e7927fcc4d5
azong76 <103876646+AZong76@users.noreply.github.com>
2022-12-04 15:18:08 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -23,7 +23,7 @@ const USER_CONFIG = {
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: 'o-sIX6LF0waRTgB7ImNG9EXDR-90',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
-      useTemplateId: 'Xr-BQqV7zz-qBu1ZqIi2Aqyu5983DQo8gGpYgTVLIDU',
+      useTemplateId: 'eVzE4s4rSWc8jXy27bmW-gOHTVG3Z-iX8ticVyprQ_Q',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
       horoscopeDate: '08-04',
       festivals: [
