6076e78a3583c1e457132a98a3066bad446b0f6b
azong76 <103876646+AZong76@users.noreply.github.com>
2022-12-04 15:23:27 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -21,9 +21,9 @@ const USER_CONFIG = {
       // 想要发送的人的名字
       name: '安琪宝贝',
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: 'o-sIX6LF0waRTgB7ImNG9EXDR-90',
+      id: 'o-sIX6PUVEsC4PdV5AVak6ezGPXk',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
-      useTemplateId: 'eVzE4s4rSWc8jXy27bmW-gOHTVG3Z-iX8ticVyprQ_Q',
+      useTemplateId: 'DNAgIi4PnXuFirpTDn-tl-5nlYsEFwid10h68PxI_MM',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
       horoscopeDate: '08-04',
       festivals: [
@@ -54,13 +54,13 @@ const USER_CONFIG = {
 
 
   // 【推送完成提醒】模板id, 用来看自己有没有发送成功的那个模板
-  CALLBACK_TEMPLATE_ID: '',
+  CALLBACK_TEMPLATE_ID: 'o-sIX6LF0waRTgB7ImNG9EXDR-90',
 
   CALLBACK_USERS: [
     {
       name: '自己',
       // 使用微信测试号：自己的微信id，扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: '',
+      id: 'o-sIX6LF0waRTgB7ImNG9EXDR-90',
     }
   ],
 
